645d4925bd0d3443ede1325b3e82e63e3b259d55
Chris Reed <chris@reedtech.us>
2024-01-02 19:22:19 -0600

    feat: enhance HealthNote SDK authentication
    - Implements token caching strategy using Laravel's Cache facade
    - Utilizes the `HeaderAuthenticator` for improved security handling
    - Adds `AuthenticatesRequests` trait for clean auth management
    - Removes dependency on `Saloon\Contracts\Authenticator`
    
    The new caching mechanism reduces the need for frequent token requests by storing the token in the cache with an appropriate TTL, thus improving the performance and reliability of the SDK. The switch to `HeaderAuthenticator` and the use of the `AuthenticatesRequests` trait streamline the authentication process and make the codebase more maintainable. This change is aimed at providing a more robust and efficient connection setup for users of the HealthNote SDK.

--- src/HealthNoteConnector.php
+++ src/HealthNoteConnector.php
@@ -2,14 +2,21 @@
 
 namespace ChrisReedIO\HealthNoteSDK;
 
-use Saloon\Contracts\Authenticator;
+use ChrisReedIO\HealthNoteSDK\Requests\GetAuthenticationToken;
+use Illuminate\Support\Facades\Cache;
+use Saloon\Exceptions\OAuthConfigValidationException;
+use Saloon\Http\Auth\HeaderAuthenticator;
 use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
+use Saloon\Traits\Auth\AuthenticatesRequests;
 use Saloon\Traits\Plugins\AcceptsJson;
+
+use function now;
 
 class HealthNoteConnector extends Connector
 {
     use AcceptsJson;
+    use AuthenticatesRequests;
 
     /**
      * {@inheritDoc}
@@ -21,11 +28,35 @@ class HealthNoteConnector extends Connector
             throw new \Exception('HealthNoteSDK base_url not set');
         }
 
-        return $baseUrl . '/api';
+        return $baseUrl.'/api';
     }
 
-    protected function defaultAuth(): TokenAuthenticator
+    public function __construct()
     {
-        return new TokenAuthenticator($this->token);
+
+    }
+
+    protected function defaultAuth(): HeaderAuthenticator
+    {
+        // Attempt to set up authentication
+        try {
+            $savedToken = Cache::get('healthnote.token');
+
+            if (!$savedToken) {
+                // Fetch a new token
+                $session = (new GetAuthenticationToken())->send()->json('session');
+                $token = $session['token'];
+                $ttl = now()->diff($session['expiresAt']);
+                Cache::put('healthnote.token', $token, $ttl);
+                $savedToken = $token;
+            }
+
+            // dd("Saved Token: $savedToken");
+            return new HeaderAuthenticator($savedToken, 'session_token');
+            // $this->authenticate($authenticator);
+        } catch (ReflectionException|OAuthConfigValidationException|Throwable $e) {
+            throw new \Exception('Athena SDK failed to authenticate: '.$e->getMessage());
+        }
+
     }
 }
